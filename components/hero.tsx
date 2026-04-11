59590cd29383b647b2fea57c249b80de8ddb2f05
Ajinkya <ajinkyamali147@gmail.com>
2026-04-11 17:05:39 +0530

    video chnage

--- components/hero.tsx
+++ components/hero.tsx
@@ -21,7 +21,7 @@ export function Hero() {
               loop
             playsInline
           >
-            <source src="/Images/Dosavideo.mp4" type="video/mp4" />
+            <source src="/Dosavideo.mp4" type="video/mp4" />
           </video>
         </div>
 
